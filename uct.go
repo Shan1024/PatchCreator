70aaf56052d6ec70dad5e6b841302dfe54aa035f
shan1024 <gambit1024@gmail.com>
2016-07-06 12:01:03 +0530

    Filename changed from main.go to uct.go

--- uct.go
+++ uct.go
@@ -4,6 +4,8 @@ import (
 	"gopkg.in/alecthomas/kingpin.v2"
 	"os"
 	"github.com/shan1024/uct/cmd"
+	"runtime"
+	"time"
 )
 
 var (
@@ -19,9 +21,13 @@ var (
 	validatePatchLoc = validateCommand.Arg("update", "Update zip location").Required().String()
 	validateDistLoc = validateCommand.Arg("dist", "Dist dir/zip location").Required().String()
 	enableLogsForValidateCommand = validateCommand.Flag("logs", "Enable debug logs").Short('l').Bool()
+
+	uctVersion = "1.0.0"
+	buildDate string
 )
 
 func main() {
+	setVersion()
 	//parse the args
 	output := kingpin.MustParse(app.Parse(os.Args[1:]))
 	//call corresponding command
@@ -31,4 +37,15 @@ func main() {
 	case validateCommand.FullCommand():
 		cmd.Validate(*validatePatchLoc, *validateDistLoc, *enableLogsForValidateCommand)
 	}
+}
+
+func setVersion() {
+	if len(buildDate) == 0 {
+		buildDate = time.Now().Format(time.UnixDate)
+	}
+	version := ("WSO2 Update Creation Tool (UCT) version: " + uctVersion + "\n")
+	version += ("Build date: " + buildDate + "\n")
+	version += ("OS\\Arch: " + runtime.GOOS + "\\" + runtime.GOARCH + "\n")
+	version += ("Go version: " + runtime.Version() + "\n\n")
+	app.Version(version)
 }

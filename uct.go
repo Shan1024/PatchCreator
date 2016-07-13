43e0f3b31226b20e4e2416c350875d57c84e2e10
shan1024 <gambit1024@gmail.com>
2016-07-13 15:34:35 +0530

    word patch changed to update

--- uct.go
+++ uct.go
@@ -13,17 +13,17 @@ var (
 	app = kingpin.New("uct", "A command-line Update Creator Tool.")
 	//Create 'create' command
 	createCommand = app.Command("create", "Create an update zip")
-	createPatchLoc = createCommand.Arg("patch", "Patch dir location").Required().String()
+	createUpdateLoc = createCommand.Arg("update", "Update dir location").Required().String()
 	createDistLoc = createCommand.Arg("dist", "Dist dir/zip location").Required().String()
 	enableDebugLogsForCreateCommand = createCommand.Flag("debug", "Enable debug logs").Short('d').Bool()
 	enableTraceLogsForCreateCommand = createCommand.Flag("trace", "Enable debug logs").Short('t').Bool()
 	//Create 'validate' command
 	validateCommand = app.Command("validate", "Validates an update zip")
-	validatePatchLoc = validateCommand.Arg("update", "Update zip location").Required().String()
+	validateUpdateLoc = validateCommand.Arg("update", "Update zip location").Required().String()
 	validateDistLoc = validateCommand.Arg("dist", "Dist dir/zip location").Required().String()
 	enableDebugLogsForValidateCommand = validateCommand.Flag("debug", "Enable debug logs").Short('d').Bool()
 	enableTraceLogsForValidateCommand = validateCommand.Flag("trace", "Enable debug logs").Short('t').Bool()
-
+	//set the default version
 	uctVersion = "1.0.0"
 	buildDate string
 )
@@ -35,12 +35,13 @@ func main() {
 	//call corresponding command
 	switch output{
 	case createCommand.FullCommand():
-		cmd.Create(*createPatchLoc, *createDistLoc, *enableDebugLogsForCreateCommand, *enableTraceLogsForCreateCommand)
+		cmd.Create(*createUpdateLoc, *createDistLoc, *enableDebugLogsForCreateCommand, *enableTraceLogsForCreateCommand)
 	case validateCommand.FullCommand():
-		cmd.Validate(*validatePatchLoc, *validateDistLoc, *enableDebugLogsForValidateCommand, *enableTraceLogsForValidateCommand)
+		cmd.Validate(*validateUpdateLoc, *validateDistLoc, *enableDebugLogsForValidateCommand, *enableTraceLogsForValidateCommand)
 	}
 }
 
+//This function sets the version details which will be displayed when --version flag is entered
 func setVersion() {
 	if len(buildDate) == 0 {
 		buildDate = time.Now().Format(time.UnixDate)
@@ -48,6 +49,6 @@ func setVersion() {
 	version := ("WSO2 Update Creation Tool (UCT) version: " + uctVersion + "\n")
 	version += ("Build date: " + buildDate + "\n")
 	version += ("OS\\Arch: " + runtime.GOOS + "\\" + runtime.GOARCH + "\n")
-	version += ("Go version: " + runtime.Version() + "\n\n")
+	version += ("Go version: " + runtime.Version() + "\n")
 	app.Version(version)
 }

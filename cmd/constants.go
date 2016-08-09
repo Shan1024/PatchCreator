9de1a53270cd6eb3b0e93aabfa6d9786b89649d6
shan1024 <gambit1024@gmail.com>
2016-08-09 12:23:31 +0530

    Unused variables removed

--- cmd/constants.go
+++ cmd/constants.go
@@ -10,9 +10,6 @@ const (
 	_INSTRUCTIONS_FILE = "instructions.txt"
 	_UPDATE_DESCRIPTOR_FILE = "update-descriptor.yaml"
 
-	//Resource directory which contains README.txt, LICENCE.txt and NOT_A_CONTRIBUTION.txt files
-	_RESOURCE_DIR = ".." + string(os.PathSeparator) + "res"
-
 	//Temporary directory to copy files before creating the new zip
 	_TEMP_DIR = "temp"
 	//This is used to store carbon.home string
@@ -21,6 +18,4 @@ const (
 	_UPDATE_DIR_ROOT = _TEMP_DIR + string(os.PathSeparator) + _CARBON_HOME
 	//Prefix of the update file and the root folder of the update zip
 	_UPDATE_NAME_PREFIX = "WSO2-CARBON-UPDATE"
-	//Prefix of the JIRA urls. This is used to generate README
-	_JIRA_URL_PREFIX = "https://wso2.org/jira/browse/"
 )

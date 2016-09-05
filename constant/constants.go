5af7a794d53371b0ee788eb092203aa915852dcb
shan1024 <gambit1024@gmail.com>
2016-09-05 20:15:11 +0530

    Remove unused constants

--- constant/constants.go
+++ constant/constants.go
@@ -24,32 +24,11 @@ const (
 	TEMP_DIR = "temp"
 	//This is used to store carbon.home string
 	CARBON_HOME = "carbon.home"
-	//Temporary directory location including carbon.home. All updated files will be copied to this location
-	UPDATE_DIR_ROOT = TEMP_DIR + string(os.PathSeparator) + CARBON_HOME
 	//Prefix of the update file and the root folder of the update zip
 	UPDATE_NAME_PREFIX = "WSO2-CARBON-UPDATE"
 
 	//Constants to store configs in viper
-	UNZIP_DIRECTORY = "_UNZIP_DIRECTORY"
 	DISTRIBUTION_ROOT = "DISTRIBUTION_ROOT"
 	UPDATE_ROOT = "UPDATE_ROOT"
 	UPDATE_NAME = "_UPDATE_NAME"
-
-	INIT_EXAMPLE = `  update_number: 0001
-  platform_version: 4.4.0
-  platform_name: wilkes
-  applies_to: All the products based on carbon 4.4.1
-  bug_fixes:
-    CARBON-15395: Upgrade Hazelcast version to 3.5.2
-    <MORE_JIRAS_HERE>
-  description: |
-    This update contain the relavent fixes for upgrading Hazelcast version
-    to its latest 3.5.2 version. When applying this update it requires a
-    full cluster estart since if the nodes has multiple client versions of
-    Hazelcast it can cause issues during connectivity.
-  file_changes:
-    added_files: []
-    removed_files: []
-    modified_files:
-    - repository/components/plugins/hazelcast_3.5.0.wso2v1.jar`
 )
